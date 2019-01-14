2bb1bca050ea64d0e2caae13ac8cbde9d762e880
devilla <dev.koold@gmail.com>
2019-01-14 17:54:54 +0530

    professional layout

--- src/CreateLink.js
+++ src/CreateLink.js
@@ -12,7 +12,7 @@ export default class CreateLink extends Component {
   }
     render(){
       return (
-        <div style={{background:'#883a75', height:'1029px'/*, width:'500px'*/}}>
+        <div style={{background:'#883a75', height:'1029px', width:'500px'}}>
         <div className="container">
         <header>
         <h1 align="center" style={{color:'white'}}>Test</h1>
@@ -21,8 +21,8 @@ export default class CreateLink extends Component {
         <div className= "col" style={{background:'#ffffff', borderRadius:'50px'}}><img src ="insta.jpeg" width="50px"/></div>
         <div  className= "col" style={{background:'#ffffff', borderRadius:'50px'}}><img src ="whatsapp.jpeg" width="50px"/></div>
         </div>
-        <div  className= "row" style={{marginTop:'20px', background:'#ffffff', borderRadius:'50px'}}><h2 style={{marginLeft:'550px',color:'#883a75'}}>Blog</h2></div>
-        <div  className= "row" style={{marginTop:'20px', background:'#ffffff', borderRadius:'50px'}}><h2 style={{marginLeft:'535px',color:'#883a75'}}>Resume</h2></div>
+        <div  className= "row" style={{marginTop:'20px', background:'#ffffff', borderRadius:'50px'}}><h2 style={{marginLeft:'200px',color:'#883a75'}}>Blog</h2></div>
+        <div  className= "row" style={{marginTop:'20px', background:'#ffffff', borderRadius:'50px'}}><h2 style={{marginLeft:'200px',color:'#883a75'}}>Resume</h2></div>
         </div>
         </div>
       );
