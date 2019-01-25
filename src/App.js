765f479290eabb88fc92a6c1d5848e4b919d5fb3
devilla <dev.koold@gmail.com>
2019-01-25 19:09:34 +0530

    Frontend login and Register

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import './assets/css/argon.css';
 import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import CreateLink from './components/CreateLink';
+import Login from './components/Login';
+import Register from './components/Register';
+
+
 
 
 class App extends Component {
@@ -13,9 +17,10 @@ class App extends Component {
     return (
       <BrowserRouter>
       <div className="App">
+      <Route path='/register' component={Register} />
+      <Route path='/login' component={Login} />
       <Route path='/dashboard' component={Homepage} />
       <Route path='/create-link' component={CreateLink} />
-      <Redirect to='/dashboard'/>
       </div>
       </BrowserRouter>
     );
